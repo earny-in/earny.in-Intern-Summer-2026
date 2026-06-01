f913818984c78241639f940910ab801990a1c40f
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-06-01 21:46:08 +0530

    Restore Day1 file

--- rajan/Main.java
+++ rajan/Main.java
@@ -1,13 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        Scanner s = new Scanner(System.in);
-
-        System.out.print("Age = ");
-        int a = s.nextInt();
-
-        System.out.println("Your age in months is " + (a * 12) + " months old.");
+        System.out.println("Hello from Rajan");
     }
 }
