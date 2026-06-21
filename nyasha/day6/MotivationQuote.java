7786fe7ff932f0cb8e97f34fad545a346a102d51
nyashagupta-24 <nyashagupta.rprn@gmail.com>
2026-06-21 15:17:05 +0530

    assignment6

---
+++ nyasha/day6/MotivationQuote.java
@@ -0,0 +1,13 @@
+public class MotivationQuote {
+
+    public static void motivation_quote() {
+        System.out.println("Today's Motivation");
+        System.out.println();
+        System.out.println("Success doesn't come from what you do occasionally.");
+        System.out.println("It comes from what you do consistently.");
+    }
+
+    public static void main(String[] args) {
+        motivation_quote();
+    }
+}
