42394edd17bebf4dc7860921b95c3d67942fd2e2
connectmeom-droid <connectme.rajom@gmail.com>
2026-06-21 23:17:43 +0530

    Day 6 issue resolved

---
+++ om/Day-6/CmpnyInfo.java
@@ -0,0 +1,16 @@
+public class CmpnyInfo {
+
+    public static void company_info() {
+         System.out.println("======================");
+        System.out.println("Company Information");
+        System.out.println("======================");
+        System.out.println("Company Name: Earny.in");
+        System.out.println("Industry: EdTech");
+        System.out.println("Location: India");
+        System.out.println("Website: www.earny.in");
+    }
+
+    public static void main(String[] args) {
+        company_info();
+    }
+}
---
+++ om/Day-6/studnt.java
@@ -0,0 +1,15 @@
+public class studnt {
+
+    public static void student_profile() {
+        System.out.println("Student Profile");
+        System.out.println();
+        System.out.println("Name: Om Raj Gupta");
+        System.out.println("Roll Number: CS106");
+        System.out.println("Branch: Computer Science");
+        System.out.println("Year: First Year");
+    }
+
+    public static void main(String[] args) {
+        student_profile();
+    }
+}
