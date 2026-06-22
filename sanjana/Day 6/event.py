783c004861807617f56acf65562c56a660a3caf2
sanjana. <sanjanakum552@gmail.com>
2026-06-22 23:38:34 +0530

    added event.py

---
+++ sanjana/Day 6/event.py
@@ -0,0 +1,12 @@
+def event_announcement():
+    print("=================================")
+    print("      EVENT ANNOUNCEMENT")
+    print("=================================")
+    print("Event Name: TechFest 2026")
+    print("Date: 15 August 2026")
+    print("Venue: Main Auditorium")
+    print("Registration Status: Open")
+    print("=================================")
+
+# Main program
+event_announcement()
