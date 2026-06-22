bdebd99cf8e1b0cbdd9440c8b7428b80e1094358
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-06-22 11:27:31 +0530

    Student profile

---
+++ rajan/Day6/Std.java
@@ -0,0 +1,13 @@
+public class Std{
+    public static void student_Profile() {
+        System.out.println("Student Profile\n");
+
+        System.out.println("Name: Aarush Maurya");
+        System.out.println("Roll Number: CS101");
+        System.out.println("Branch: Computer Science");
+        System.out.println("Year: First Year");
+    }
+    public static void main(String[] args) {
+        student_Profile();
+    }
+}
