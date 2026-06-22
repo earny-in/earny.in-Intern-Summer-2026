edbf435030e97f31e98e17c2871bd27350ebcef9
sanjana. <sanjanakum552@gmail.com>
2026-06-22 23:49:48 +0530

    Added intern.py

---
+++ sanjana/Day 7/intern.py
@@ -0,0 +1,9 @@
+def welcome_intern(name):
+    print(f"Welcome {name}!\n")
+    print("We are excited to have you as part of our internship program.")
+
+# Accept input from user
+name = input()
+
+# Call the function
+welcome_intern(name)
