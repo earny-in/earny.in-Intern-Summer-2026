c7fd985c94726212e8755ef8896936cb9206f48d
sanjana. <sanjanakum552@gmail.com>
2026-06-23 00:03:56 +0530

    Added Reg.py

---
+++ sanjana/Day 7/Reg.py
@@ -0,0 +1,12 @@
+def event_registration(student_name, event_name):
+    print("\nEvent Registration\n")
+    print("Student Name:", student_name)
+    print("Event Name:", event_name)
+    print("Status: Successfully Registered")
+
+# User input
+student_name = input("Student Name: ")
+event_name = input("Event Name: ")
+
+# Function call
+event_registration(student_name, event_name)
