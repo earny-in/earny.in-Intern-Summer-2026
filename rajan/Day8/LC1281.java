ced1facb74a708231c938166092305dd8bd0768b
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-07-05 13:19:19 +0530

    Add LeetCode 1281 solution

---
+++ rajan/Day8/LC1281.java
@@ -0,0 +1,21 @@
+public class LC1281 {
+
+    public static int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 234;
+        System.out.println(subtractProductAndSum(n));
+    }
+}
