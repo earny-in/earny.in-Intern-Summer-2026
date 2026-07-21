fa798c0bbe14206ad7f1c7b1e58349a8a0fb735d
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-07-21 19:10:32 +0530

    slove day 10 number triange pattern

--- rajan/Day10/tri-pattern.py
+++ rajan/Day10/tri-pattern.py
@@ -1,6 +1,6 @@
-n = int(input("Enter range: "))
+n = int(input("Enter triangle range: "))
 
-for i in range(n,0 ,-1):
-    for j in range(i):
-        print("*", end="")
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
     print()
